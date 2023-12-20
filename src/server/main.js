a936d7cc90bb08f6dc80e5cca15c0bd898352346
Peng Cui <pengcui@Pengs-MacBook-Pro.local>
2023-12-20 08:25:12 -0800

    second commit

--- src/server/main.js
+++ src/server/main.js
@@ -1,5 +1,5 @@
-import express from "express";
-import ViteExpress from "vite-express";
+const express = require ("express");
+const ViteExpress = require ("vite-express");
 
 const app = express();
 
@@ -7,6 +7,6 @@ app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
-ViteExpress.listen(app, 3000, () =>
+ViteExpress.listen(app, 4500, () =>
   console.log("Server is listening on port 3000..."),
 );
--- src/server/seed.js
+++ src/server/seed.js
@@ -3,12 +3,12 @@ const prisma = new PrismaClient();
 
 const seed = async () =>{
    const usersData = [
-    {signup_date:'12/15/2021', username:'user1', password:'password1', email:'user1@email.com', phone:'123-456-7890', first_name:'John', last_name:'Doe', can_sell:'true', address:'110 main street'},
-    {signup_date:'12/16/2021', username:'user2', password:'password2', email:'user2@email.com', phone:'234-567-8901', first_name:'Jason', last_name:'Smith', can_sell:'true', address:'220 oak street'},
-    {signup_date:'12/17/2021', username:'user3', password:'password3', email:'user3@email.com', phone:'345-678-9012', first_name:'Tom', last_name:'James', can_sell:'true', address:'330 market street'},
-    {signup_date:'12/18/2021', username:'user4', password:'password4', email:'user4@email.com', phone:'456-789-0123', first_name:'Fred', last_name:'Lincoln', can_sell:'false', address:'440 oakland street'},
-    {signup_date:'12/19/2021', username:'user5', password:'password5', email:'user5@email.com', phone:'567-890-1234', first_name:'Jake', last_name:'Carter', can_sell:'false', address:'550 queen street'},
-    {signup_date:'12/20/2021', username:'user6', password:'password6', email:'user6@email.com', phone:'678-901-2345', first_name:'Mike', last_name:'Grayson', can_sell:'false', address:'660 sunset boulevard'},  
+    {signup_date:'12/15/2021', username:'user1', password:'password1', email:'user1@email.com', phone:'123-456-7890', first_name:'John', last_name:'Doe', can_sell:'true', address:'110 main street', addressline2:'apt1', city:'Los Angeles', state:'CA', zip:'90015'},
+    {signup_date:'12/16/2021', username:'user2', password:'password2', email:'user2@email.com', phone:'234-567-8901', first_name:'Jason', last_name:'Smith', can_sell:'true', address:'220 oak street',addressline2:'apt2', city:'San Francisco', state:'CA', zip:'94158'},
+    {signup_date:'12/17/2021', username:'user3', password:'password3', email:'user3@email.com', phone:'345-678-9012', first_name:'Tom', last_name:'James', can_sell:'true', address:'330 market street',addressline2:'apt3', city:'Iowa city', state:'IA', zip:'52245'},
+    {signup_date:'12/18/2021', username:'user4', password:'password4', email:'user4@email.com', phone:'456-789-0123', first_name:'Fred', last_name:'Lincoln', can_sell:'false', address:'440 oakland street',addressline2:'apt4', city:'Cincinnati', state:'OH', zip:'45203'},
+    {signup_date:'12/19/2021', username:'user5', password:'password5', email:'user5@email.com', phone:'567-890-1234', first_name:'Jake', last_name:'Carter', can_sell:'false', address:'550 queen street',addressline2:'apt5', city:'Boston', state:'MA', zip:'02108'},
+    {signup_date:'12/20/2021', username:'user6', password:'password6', email:'user6@email.com', phone:'678-901-2345', first_name:'Mike', last_name:'Grayson', can_sell:'false', address:'660 sunset boulevard',addressline2:'apt6', city:'New York', state:'NY', zip:'10002'},  
 ];
 
 await prisma.user.createMany({
