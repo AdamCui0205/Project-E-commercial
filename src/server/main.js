f3a22769c8b27f98d89c83f73b74a7ac225ca59c
Blake <bgeard@wgu.edu>
2023-12-21 13:21:07 -0500

    updates main.js toi includer products/users routes

--- src/server/main.js
+++ src/server/main.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000;
 
 const cartItemsRoutes = require('./api/cartItems');
 const ordersRoutes = require('./api/orders');
-// const productsRoutes = require('./api/products');
-// const usersRoutes = require('./api/users');
+const productsRoutes = require('./api/products');
+const usersRoutes = require('./api/users');
 const authRoutes = require('./auth/auth');
 // const authenticateToken = require('./auth/authenticateToken');
 
@@ -16,8 +16,8 @@ app.use(express.json());
 // Use the imported routes
 app.use('/api/cart-items', cartItemsRoutes);
 app.use('/api/orders', ordersRoutes);
-// app.use('/api/products', productsRoutes);
-// app.use('/api/users', usersRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/users', usersRoutes);
 app.use('/auth', authRoutes);
 
 // app.get('/protected-route', authenticateToken, (req, res) => {
