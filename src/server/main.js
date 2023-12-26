510141a8f225e01b2ce522109a796d5806e3c919
Blake <bgeard@wgu.edu>
2023-12-26 15:12:13 -0500

    adds 404 and catch all to server

--- src/server/main.js
+++ src/server/main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
+
 const port = process.env.PORT || 3000;
 
 const cartItemsRoutes = require('./api/cartItems');
@@ -10,12 +12,14 @@ const usersRoutes = require('./api/users');
 const authRoutes = require('./auth/auth');
 
 require('dotenv').config();
+
 // Middleware for CORS
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}))
+}));
+
 // Middleware for parsing body of incoming requests
 app.use(express.json());
 
@@ -26,6 +30,21 @@ app.use('/api/products', productsRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/auth', authRoutes);
 
+const viteBuildPath = path.join(__dirname, 'dist');
+
+app.use(express.static(viteBuildPath));
+
+// The catch-all route: for any request that doesn't match other routes,
+// send back the app's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(viteBuildPath, 'index.html'));
+});
+
+// Handle 404 errors
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(viteBuildPath, 'index.html'));
+});
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
