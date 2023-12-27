b2d09c4e4dfe6b14182d37063483d6d8d6f3313b
Blake <bgeard@wgu.edu>
2023-12-26 21:03:51 -0500

    styles login modal

--- src/client/components/LoginModal.jsx
+++ src/client/components/LoginModal.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
+import '../styles/LoginModal.css';
 
 const LoginModal = ({ isOpen, onClose }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        // TODO: Login logic will go here
+        console.log("Logging in:", { email, password });
+        onClose(); // Close modal after login is complete. TODO: Handle errors
+    };
+
     return (
-        <Modal isOpen={isOpen} onRequestClose={onClose} contentLabel="Login">
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onClose}
+            className="loginModal"
+            contentLabel="Login"
+        >
             <h2>Login</h2>
-            {/* Login form goes here */}
-            <button onClick={onClose}>Close</button>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+                <button type="button" onClick={onClose}>Cancel</button>
+            </form>
         </Modal>
     );
 };
