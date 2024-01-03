b242450772c60aaf069f9af179bcda38b6de03a1
aflores004 <aflores004@citymail.cuny.edu>
2024-01-03 14:45:58 -0500

    testing

--- src/client/components/ProductCard.jsx
+++ src/client/components/ProductCard.jsx
@@ -6,7 +6,7 @@ export default function ProductCard(product){
         <h2>{product.title}</h2>
         <img src={product.image_url} alt={product.image_url}/>
         <p>{product.description}</p>
-        <p>Date Posted: {"productInfo.post_date"}</p>
+        <p>Date Posted: {product.post_date.toLocaleString()}</p>
         <p>Category: {product.category}</p>
         <p>Price: {product.price}</p>
         <p>Available: {product.is_available ? ("Yes") : ("No")}</p>
