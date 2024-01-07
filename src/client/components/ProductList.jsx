1ead92b7a8966fa04ce6004188e639dd133bfe2f
Blake <bgeard@wgu.edu>
2024-01-07 18:50:04 -0500

    adds search functionality

--- src/client/components/ProductList.jsx
+++ src/client/components/ProductList.jsx
@@ -4,21 +4,34 @@ import '../styles/ProductList.css';
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         fetch('https://cache-corner.onrender.com/api/products')
             .then(response => response.json())
             .then(data => {
-                console.log("Fetched products:", data); // Log fetched products
-                setProducts(data);
+                console.log("Fetched products:", data);
+                const filteredProducts = data.filter(product =>
+                    product.title.toLowerCase().includes(searchQuery.toLowerCase())
+                );
+                setProducts(filteredProducts);
             })
             .catch(error => console.error('Error fetching products:', error));
-    }, []);
+    }, [searchQuery]);
+
+    const handleSearchChange = event => {
+        setSearchQuery(event.target.value);
+    };
 
     return (
         <div>
             <div className="search-container">
-                <input type="text" placeholder="Search..." />
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                />
             </div>
             <div className="product-list-container">
                 {products.length > 0 ? (
@@ -26,7 +39,7 @@ const ProductList = () => {
                         <ProductCard key={product.product_id} product={product} />
                     ))
                 ) : (
-                    <p>No products found.</p> // Display this message if there are no products
+                    <p>No products found.</p>
                 )}
             </div>
         </div>
