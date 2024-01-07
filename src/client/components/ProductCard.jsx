d2548251ad7200c04cd9abf515e5ea7cee55ea34
Gregory Jean Baptiste <gregster0@gmail.com>
2024-01-07 00:21:32 -0500

    product card shortened

--- src/client/components/ProductCard.jsx
+++ src/client/components/ProductCard.jsx
@@ -12,11 +12,7 @@ const ProductCard = ({ product }) => {
         <section>
             <h2>{product.title}</h2>
             <img src={product.image_url} alt={`${product.title} Image`} />
-            <p>{product.description}</p>
-            <h3>Date Posted: {new Date(product.post_date).toDateString()}</h3>
-            <h3>Category: {product.category}</h3>
             <h3>Price: ${product.price.toFixed(2)}</h3>
-            <h3>Available: {product.is_available ? "Yes" : "No"}</h3>
             <button onClick={handleMoreInfoClick}>More Info!</button>
         </section>
     );
