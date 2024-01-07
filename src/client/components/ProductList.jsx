d6e50e1878028753c89ea3766ba5f90b60bacd23
Blake <bgeard@wgu.edu>
2024-01-07 06:22:51 -0500

    styles the all products

--- src/client/components/ProductList.jsx
+++ src/client/components/ProductList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import ProductCard from './ProductCard'; // Import the ProductCard.css component
+import ProductCard from './ProductCard';
+import '../styles/ProductList.css';
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
@@ -14,17 +15,18 @@ const ProductList = () => {
 
     return (
         <div>
-        <div className="search-container">
-            <input type="text" placeholder="Search..." />
-        </div>
-
-            {products.length > 0 ? (
-                products.map(product => (
-                    <ProductCard key={product.product_id} product={product} />
-                ))
-            ) : (
-                <p>No products found.</p> // Display this message if there are no products
-            )}
+            <div className="search-container">
+                <input type="text" placeholder="Search..." />
+            </div>
+            <div className="product-list-container">
+                {products.length > 0 ? (
+                    products.map(product => (
+                        <ProductCard key={product.product_id} product={product} />
+                    ))
+                ) : (
+                    <p>No products found.</p> // Display this message if there are no products
+                )}
+            </div>
         </div>
     );
 };
