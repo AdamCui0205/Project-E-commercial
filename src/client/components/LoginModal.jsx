3eb05f24ba02cec98e2aa5988c432aa7741ecda7
Blake <bgeard@wgu.edu>
2024-01-07 23:14:32 -0500

    fixes login

--- src/client/components/LoginModal.jsx
+++ src/client/components/LoginModal.jsx
@@ -14,7 +14,7 @@ const LoginModal = ({ isOpen, onClose, setUserId }) => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:3000/auth/login', {
+            const response = await fetch('https://cache-corner.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
